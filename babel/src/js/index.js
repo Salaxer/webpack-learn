549cfb62016b6b395a98ca80aacd52568f32a750
Salaxer <hector_salazar12@hotmail.com>
2021-02-10 15:36:56 -0600

    he terminado de configurar babel para que mi codigo funcione en todos los navegadores

--- babel/src/js/index.js
+++ babel/src/js/index.js
@@ -1,10 +1,24 @@
 
-import '../css/index.css'
-import text from './text';
-text();
-if(module.hot){
-    module.hot.accept('./text.js', function () {
-        console.log('He recargado en caliente');
-        text();
-    })
-}
+import '../css/index.css';
+import search from './search';
+import render from './render';
+
+const id = prompt('Quien es ese poquemon')
+
+search(id)
+.then((data)=>{
+    render(data);
+}).catch(()=>{
+    console.log('No hubo pokemon');
+})
+
+
+
+// import text from './text';
+// text();
+// if(module.hot){
+//     module.hot.accept('./text.js', function () {
+//         console.log('He recargado en caliente');
+//         text();
+//     })
+// }
