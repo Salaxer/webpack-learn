9e606fbc1a6e09a9a55c908417c45d8cfb37cf14
Salaxer <hector_salazar12@hotmail.com>
2021-02-10 10:49:32 -0600

    comenzando con babel

---
+++ plugin-css/src/js/index.js
@@ -0,0 +1,4 @@
+
+import '../css/index.css'
+
+document.body.innerHTML = ('Hola mundo desde webpack');
---
+++ plugin-css/webpack.config.js
@@ -0,0 +1,47 @@
+// importamos un modulo
+//Nos permite acceder a donde estámos en las carpetas. Ya sea en local o en la nube.
+const path = require('path');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports={
+    // Todo lo que estare anexando a la entrada, este caso solo es el archivo index.js
+    entry:{
+        home: path.resolve(__dirname,'src','js','index.js')
+    } ,
+    // seleccionamos el modo
+    mode: 'development',
+    output: {
+        // devuelve rutas absolutas, partiendo de la ruta actual y manda mi archivo final ahi
+        // con una coma despues del __dirname podemos colocar as carpetas dentro de nuestro servidor, comunmente se usa dist
+        path: path.resolve(__dirname,'dist'),
+        // ahora tenemos que nombrar nuestro archivo final
+        filename: 'js/[name].js'
+    },
+    // Para agregar modulos
+    module:{
+        rules:[
+            {
+                // Aqui le estoy diciendo que acepte cualquier archivo que termine en css. sera interpretado
+                test: /\.css$/,
+                // aqui usamos algunas dependencias que se rquiere para aceptar codigo css en js
+                // el css-loader sirve para que mi codigo no se rompa y el otro sirve para injectar el codigo en mi js
+                // primero se ejecutan los ultimos, que en este caso es el css-loader y despues se ejecuta el style-loader
+                // va de derecha a izquierda 
+                use: [{
+                    loader: MiniCSSExtractPlugin.loader
+                },
+                'css-loader'],
+            }
+        ]
+    },
+    // este plugin sirve para exportar un archivo css en un archivo diferente
+    plugins:[
+        new HtmlWebpackPlugin({
+            title: 'Plugins',
+            template: path.resolve(__dirname, 'index.html')
+        }),
+        new MiniCSSExtractPlugin({
+            filename: 'css/[name].css',
+        })
+    ]
+}
